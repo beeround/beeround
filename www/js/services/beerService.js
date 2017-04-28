c48d1328d45fb1a1d9d5ca6bff32572d98ab6934
Karsten Schütz <karstenschuetz@web.de>
2017-04-28 13:07:39 +0200

    Added API Brewery Query

--- www/js/services/beerService.js
+++ www/js/services/beerService.js
@@ -2,12 +2,19 @@ angular.module('beeround.services', [])
   .service('beerService', ['$http', '$q',
     function ($http, $q) {
 
-
       this.getBeers = function () {
         return $http.get('http://api.brewerydb.com/v2/breweries?key=7802f26125b23378098b3c32911adcce').then(function (res) {
           return res.data;
         });
       };
     }
+
+    // function ($http, $q) {
+    //   this.getBrewerys = function () {
+    //     return $http.get('http://api.brewerydb.com/v2/locations?postalCode='+ $scope.address.zip +'&key=7802f26125b23378098b3c32911adcce').then(function (res) {
+    //       return res.data;
+    //     });
+    //   };
+    // }
 
 ]);
