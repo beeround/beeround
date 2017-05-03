c24580bdda2b82e83a05e71398d4f5b18d613e3d
dominikspeh <mail@domi-speh.de>
2017-05-03 18:43:29 +0200

    Added marker to map view

--- www/js/controllers/beer.js
+++ www/js/controllers/beer.js
@@ -199,33 +199,38 @@ angular.module('beeround.beer', [])
 
       let mapOptions = {
         center: latLng,
-        zoom: 15,
+        zoom: 9,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
       $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+      //GET Breweries around
+      beerService.getBreweriesNearCoordinates(position.coords.latitude, position.coords.longitude, 40).then(location => {
+
+        let infowindow = new google.maps.InfoWindow;
+
+        location.data.map((result,index) => {
+          let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(result.latitude, result.longitude),
+            map: $scope.map,
+          });
+
+          //Add marker
+          google.maps.event.addListener(marker, 'click', (function(marker) {
+            return function() {
+              infowindow.setContent(result.brewery.name);
+              infowindow.open(map, marker);
+            }
+          })(marker));
+        });
+
+
+      });
+
       // Hide loading
       $ionicLoading.hide();
 
-      //Wait until the map is loaded
-      google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-        let marker = new google.maps.Marker({
-          map: $scope.map,
-          animation: google.maps.Animation.DROP,
-          position: latLng
-        });
-
-        let infoWindow = new google.maps.InfoWindow({
-          content: "Here I am!"
-        });
-
-        google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.open($scope.map, marker);
-        });
-
-      });
 
 
     }, function(error){
