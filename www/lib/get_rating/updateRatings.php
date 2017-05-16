f729e7bbf30b058e7c50a46217034309e7ceec7a
Karsten Schütz <karstenschuetz@web.de>
2017-05-16 12:25:39 +0200

    Added ratings

--- www/lib/get_rating/updateRatings.php
+++ www/lib/get_rating/updateRatings.php
@@ -1,17 +1,21 @@
 <?php
-    $host_name  = "db680429890.db.1and1.com";
-    $database   = "db680429890";
-    $user_name  = "dbo680429890";
-    $password   = "on14-beeroundTheWorld";
 
-$beerid='17uR4';
+require 'DatabaseHelper.php';
 
-$conn = new mysqli($host_name, $user_name, $password, $database);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+class Ratings
+{
+    function updateRatings($beerId)
+    {
+        $database = new DatabaseHelper();
+        $avgRatings = $database->getValues("SELECT beerid, AVG(rating) 
+                                      FROM ratings 
+                                      WHERE beerid = ('" . $beerId . "')");
+        foreach ($avgRatings as $avgRating) {
+            echo $avgRating . PHP_EOL;
+        }
+    }
+
 }
 
-$avg = $conn->query("SELECT beerid, AVG(rating) FROM ratings WHERE beerid = ('" . $beerid . "')");
-$row = $avg->fetch_assoc();
-echo json_encode($row);
-mysqli_close($conn);
+$shell = new Ratings();
+$shell->updateRatings('17uR4');
