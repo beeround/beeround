efbe14e66c749d174cdeca3a362ad2c46ce3068a
Andreas Kraft <kraftandreas@me.com>
2017-05-16 12:00:02 +0200

    API + DB

--- www/lib/get_rating/updateRatings.php
+++ www/lib/get_rating/updateRatings.php
@@ -4,40 +4,14 @@
     $user_name  = "dbo680429890";
     $password   = "on14-beeroundTheWorld";
 
+$beerid='17uR4';
 
-    // // Create connection
-    // $conn = mysqli_connect($host_name, $user_name, $password, $database);
-    // // Check connection
-    // if (!$conn) {
-    //     die("Connection failed: " . mysqli_connect_error());
-    // }
+$conn = new mysqli($host_name, $user_name, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
-//     $beerid = '17uR4';
-//
-//
-//    function getAverage($beerid){
-//     // $beerid = mysqli_real_escape_string($conn, $beerid);
-//
-//     $result = mysqli_query($conn, "SELECT beerid, AVG(rating) FROM ratings WHERE beerid=('$beerid');");
-//       print_r ($result);
-//
-//       while($row = mysqli_fetch_assoc($result)) {
-//         echo "beerid: " . $row["beerid"]. " - average rating: " . $row["AVG(rating)"]. "<br>";
-//       }
-//        /* free result set */
-//        mysqli_free_result($result);
-//
-//     }
-// getAverage("17uR4");
-// //
-
-
-    $conn = new mysqli($host_name, $user_name, $password, $database);
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-    $beerid = '17uR4';
-    $avg = $conn->query("SELECT beerid, AVG(rating) FROM ratings WHERE beerid=('".$beerid."')");
-    $row = $avg->fetch_assoc();
-    echo json_encode($row);
-    mysqli_close($conn);
+$avg = $conn->query("SELECT beerid, AVG(rating) FROM ratings WHERE beerid = ('" . $beerid . "')");
+$row = $avg->fetch_assoc();
+echo json_encode($row);
+mysqli_close($conn);
